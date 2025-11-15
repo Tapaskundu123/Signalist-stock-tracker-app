d1dd6811546c0a3c03cb85ec1b050ac2391d1c46
Tapas kundu <tapaskundu3762@gmail.com>
2025-11-15 23:06:06 +0530

    adding chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+import TradingWidget from "@/components/TradingWidget";
+import { MARKET_OVERVIEW_WIDGET_CONFIG } from "@/lib/constants";
 
-import { Button } from "@/components/ui/button"
 
 export default function Home() {
  return(
   <div className="flex min-h-screen home-wrapper">
-    <Button>Click</Button>
+    <section>
+      <div>
+        <TradingWidget 
+        title="Market Overview"
+        scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js"
+        config={MARKET_OVERVIEW_WIDGET_CONFIG}
+        className="custom-chart"
+        />
+      </div>
+    </section>
   </div>
  )
 }
+
+
+
